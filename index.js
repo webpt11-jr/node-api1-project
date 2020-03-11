35137440253c1f99c3089ba1a13f8e75a65f6f71
juniordugue <jrdugue@gmail.com>
2020-03-11 09:06:29 -0400

    installed express, added boilerplate to index, connecting to the db, importing express, listening to server and added express.json

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+
+server.listen(4001, () => {
+    console.log('listening on port 4001');
+})
+
+server.use(express.json());
