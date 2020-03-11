99d82d52e1f3614b6d74b1162a03ac2f10148fd7
juniordugue <jrdugue@gmail.com>
2020-03-11 09:29:26 -0400

    installed cors and imported cors in index file, also sent a get request via h1 tag so i can see  the request in postman

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // implement your API here
+
+// referencing our express module
 const express = require('express');
+const cors = require('express');
+
+// 
 const db = require('./data/db');
 
+//server object required to listen
 const server = express();
 
+//callback needed to listen via console
 server.listen(4001, () => {
     console.log('listening on port 4001');
 })
 
 server.use(express.json());
+server.use(cors());
+
+server.get('/', (req,res) => {
+    res.send(`<h1>Jr's Node API 1 Project</h1>`);
+})
